6a14dfd5f5d5797b0b1608e58c4c686ed98cf2b7
Jayanthi Sharma <jayanthi.bsr@gmail.com>
2018-08-21 14:48:28 +1000

    Initial Commit

---
+++ src/test/java/com/selenium/automation/helloworldTest.java
@@ -0,0 +1,12 @@
+package com.selenium.automation;
+
+import org.testng.annotations.Test;
+
+public class helloworldTest {
+
+	@Test
+	public void test()
+	{
+		System.out.println("Helloworld");
+	}
+}
